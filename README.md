65bbcc2d7b02889e325cf8e5cd2b02a6673df882
Fabio Gomes Rocha <gomesrocha@gmail.com>
2021-04-13 19:57:57 -0300

    Atualização e criação de OCR com temporizador

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DigitalSE-OCR-Python
+Estudo sobre OCR em Python e API de OCR desenvolvida em Python com Flask
--- src/ocr2.py
+++ src/ocr2.py
@@ -10,13 +10,13 @@ ap.add_argument("-d", "--digits", type=int, default=0, help="tipos de digitos")
 args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
-rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 options = ""
 if args["digits"] > 0:
     options = "outputbase digits"
 t = time.process_time()
-text = pytesseract.image_to_string(rgb, config=options)
+text = pytesseract.image_to_string(gray, config=options)
 elapsed_time = time.process_time() - t
 print(elapsed_time)
 print(text)
