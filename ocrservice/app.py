b4a2b6aad9ac25161ce672ac778cd3038c0711be
Fabio Rocha <gomesrocha@gmail.com>
2024-02-20 17:15:05 -0300

    Install Tesseract OCR and Minio, add code to extract text from images using Tesseract and save the extracted text.

--- ocrservice/app.py
+++ ocrservice/app.py
@@ -1,8 +1,19 @@
 from faststream import FastStream
 from faststream.rabbit import RabbitBroker
+from minio import Minio
+from io import BytesIO
+from PIL import Image
+import pytesseract
 
 broker = RabbitBroker("amqp://guest:guest@rabbitmq:5672")
 app = FastStream(broker)
+
+minio_bucket = "images"
+
+minio_client = Minio("minio:9000",
+                      access_key="digitalse",
+                      secret_key="digitalse",
+                      secure=False)
 
 
 
@@ -11,3 +22,13 @@ app = FastStream(broker)
 async def handle(image_name: str):
     print("Received alerts")
     print(image_name)
+    objeto = minio_client.get_object(minio_bucket, image_name)
+    imagem_bytes = objeto.read()
+
+    imagem = Image.open(BytesIO(imagem_bytes))
+
+    # Extrair texto usando Tesseract
+    texto_extraido = pytesseract.image_to_string(imagem)
+
+    # Imprimir o texto extraído
+    print(texto_extraido)
