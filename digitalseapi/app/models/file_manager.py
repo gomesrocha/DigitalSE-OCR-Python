11cce9ee74d3096b2a31c40a0f539da364a38fa5
Fabio Rocha <gomesrocha@gmail.com>
2024-02-28 21:27:50 -0300

    Refactor file manager to support word search

--- digitalseapi/app/models/file_manager.py
+++ digitalseapi/app/models/file_manager.py
@@ -1,7 +1,12 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 from typing import List, Optional
 from datetime import datetime
 from pydantic import BaseModel
+
+
+class Token(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    word: str
 
 
 class GestaoArquivos(SQLModel, table=True):
@@ -14,3 +19,30 @@ class GestaoArquivos(SQLModel, table=True):
     responsavel: Optional[str] = Field(max_length=255)
     data: Optional[datetime] = Field(default=datetime.now())
     localizacao: Optional[str] = Field(max_length=255)
+    tokens: List["Token"] = Relationship(back_populates="GestaoArquivos")
+
+
+# Definir modelo para a tabela de associação entre documentos e tokens
+class DocumentToken(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    document_id: int = Field(foreign_key="GestaoArquivos.id")
+    token_id: int = Field(foreign_key="token.id")
+
+    # Relacionamentos
+    document: Optional["GestaoArquivos"] = Relationship(back_populates="tokens_association")
+    token: Optional["Token"] = Relationship(back_populates="documents_association")
+
+
+# Adicionar relação muitos para muitos entre Document e Token
+GestaoArquivos.tokens_association = Relationship(
+    to=Token,
+    secondary=DocumentToken.__tablename__,
+    back_populates="GestaoArquivos"
+)
+
+# Adicionar relação muitos para muitos entre Token e Document
+Token.documents_association = Relationship(
+    to=GestaoArquivos,
+    secondary=DocumentToken.__tablename__,
+    back_populates="tokens"
+)
