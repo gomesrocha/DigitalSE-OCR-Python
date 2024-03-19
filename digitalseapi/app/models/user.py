cdd0b8905842d855133b2ff1d9a115b7ccfdf8bf
Fabio Rocha <gomesrocha@gmail.com>
2024-03-19 11:17:54 -0300

    Update security Dockerfile #7

--- digitalseapi/app/models/user.py
+++ digitalseapi/app/models/user.py
@@ -9,12 +9,9 @@ class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     email: str = Field(unique=True, nullable=False)
     username: str = Field(unique=True, nullable=False)
-    avatar: Optional[str] = None
-    bio: Optional[str] = None
     password: str = Field(nullable=False)
     name: str = Field(nullable=False)
     dept: str = Field(nullable=False)
-    currency: str = Field(nullable=False)
 
     @property
     def superuser(self):
